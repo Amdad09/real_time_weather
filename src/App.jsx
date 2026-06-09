c6caa24efe00fea515a550dd8ce87556191b34d6
amdadul haque <amdadulhaque601671@gmail.com>
2026-06-09 22:22:44 +0600

    competed header ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import bg from './assets/body-bg.png';
+import Header from './components/header/Header';
 function App() {
-  return (
-    <div className="underline">real time weather</div>
-  )
+    return (
+        <div
+            className="bg-body font-[Roboto] text-light bg-no-repeat bg-cover h-screen grid place-items-center"
+            style={{
+                backgroundImage: `url(${bg})`,
+            }}
+      >
+        
+
+            <Header />
+        
+        </div>
+    );
 }
-export default App
+export default App;
